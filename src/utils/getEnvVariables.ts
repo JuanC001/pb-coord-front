21da44c6a01ca7e39db6a2df72e39897428cf8c0
Juan Camilo Florez Paez <jcflorezpaez@gmail.com>
2025-04-19 18:43:08 -0500

    feat: se añade extención con google maps

--- src/utils/getEnvVariables.ts
+++ src/utils/getEnvVariables.ts
@@ -1,5 +1,6 @@
 export const getEnvVariables = () => {
     return {
-        API_URL: import.meta.env.VITE_API_URL
+        API_URL: import.meta.env.VITE_API_URL,
+        VITE_GOOGLE_MAPS_API_KEY: import.meta.env.VITE_GOOGLE_MAPS_API_KEY || '',
     }
 }
